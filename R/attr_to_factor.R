e6134a540a988b2c648c104ff0514af21f31075c
jon-nat <62013249+jon-nat@users.noreply.github.com>
2022-03-25 16:15:27 +0100

    "Closes #3" lagt till funktion get_data (#4)
    Co-authored-by: Jonatan Nåtman <jonna@rcvg.local>

--- R/attr_to_factor.R
+++ R/attr_to_factor.R
@@ -5,8 +5,8 @@
 #' @return Factor
 #' @examples
 #' df <- structure(
-#'   data.frame(cars = 1:3), 
-#'   map = data.frame(levels = 1:3, labels = c("Volvo", "Saab", "Opel")
+#'   data.frame(cars = 1:3),
+#'   map = data.frame(levels = 1:3, labels = c("Volvo", "Saab", "Opel"))
 #' )
 #' attr_to_factor(df$cars)
 #' @export
